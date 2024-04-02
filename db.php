05d7f787dd36fb3803162c9337f4b119fc988f60
Mohamed Mushkir <mushkirmohamed@gmail.com>
2024-04-02 16:58:51 +0530

    first commit

---
+++ db.php
@@ -0,0 +1,102 @@
+<?php
+// Model (MVC)
+class Database
+{
+
+    private $dsn = "mysql:host=localhost;dbname=php_oops_crud;";
+    private $user = "root";
+    private $password = "";
+    public $conn;
+
+    public function __construct()
+    {
+        try {
+            $this->conn = new PDO($this->dsn, $this->user, $this->password);
+        } catch (PDOException $ex) {
+            echo "Error: " . $ex->getMessage();
+        }
+    }
+
+    public function insert($fname, $lname, $email, $phone)
+    {
+        $sql = "INSER INTO users (first_name, last_name, email, phone) VALUES (:fname, :lname, :email, :phone)";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->execute(['fname' => $fname, 'lname' => $lname, 'email' => $email, 'phone' => $phone]);
+
+        return true;
+    }
+
+
+    public function read()
+    {
+        $data = array();
+
+        $sql = "SELECT * FROM users";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->execute();
+
+        $result = $stmt->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach ($result as $row) {
+            $data[] = $row;
+        }
+
+        return $data;
+    }
+
+    public function getUserById($id)
+    {
+        $sql = "SELECT * FROM users WHERE id = :id";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->execute(['id' => $id]);
+
+        $result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        return $result;
+    }
+
+    public function update($id, $fname, $lname, $email, $phone)
+    {
+        $sql = "UPDATE users SET first_name = :fname, last_name = :lname, email = :email, phone = :phone WHERE id = :id";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->execute(['fname' => $fname, 'lname' => $lname, 'email' => $email, 'phone' => $phone, 'id' => $id]);
+
+        return true;
+    }
+
+    public function delete($id)
+    {
+        $sql = "DELETE FROM users WHERE id = :id";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->execute(['id' => $id]);
+
+        return true;
+    }
+
+    public function totalRowCount()
+    {
+        $sql = "SELECT * FROM users";
+
+        $stmt = $this->conn->prepare($sql);
+
+        $stmt->execute();
+
+        $t_rows = $stmt->rowCount();
+
+        return $t_rows;
+    }
+}
+
+$db = new Database();
+// $array = $db->read();
+// print_r($array);
---
+++ index.php
@@ -0,0 +1,175 @@
+<?
+// View (MVC)
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP CRUD App</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css">
+
+    <!-- Data Table CDN -->
+    <link href="https://cdn.datatables.net/v/bs5/dt-2.0.3/datatables.min.css" rel="stylesheet">
+
+    <!-- Bootstrap CSS CDN -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css">
+</head>
+
+<body>
+
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-secondary">
+        <div class="container-fluid">
+            <a class="navbar-brand text-light " href="#">Navbar</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo02" aria-controls="navbarTogglerDemo02" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active text-light " aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light " href="#">Blog</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light " href="#">About</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link text-light " href="#">Contact</a>
+                    </li>
+
+                </ul>
+
+            </div>
+        </div>
+    </nav>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12">
+                <h4 class=" text-center text-danger fw-normal my-3">CRUD Application using Bootstrap, PHP - OOPs, PDO - MySQL, Ajax, DataTable & SweetAlert</h4>
+            </div>
+        </div>
+
+        <div class="row">
+            <div class="col-lg-6">
+                <h4 class="mt-2 text-primary ">All Users in database</h4>
+            </div>
+            <div class="col-lg-6">
+                <button type="button" class="btn btn-primary m-1 float-end" data-bs-toggle="modal" data-bs-target="#addModal">Add New User</button>
+                <a href="#" class=" btn btn-success m-1 float-end "> Export to Excel</a>
+            </div>
+        </div>
+
+        <hr class="my-1 ">
+
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="table-responsive" id="showUser">
+                    <table class="table table-striped table-sm table-bordered ">
+                        <thead>
+                            <tr>
+                                <th class="text-center">ID</th>
+                                <th class="text-center">First Name</th>
+                                <th class="text-center">Last Name</th>
+                                <th class="text-center">Email</th>
+                                <th class="text-center">Phone</th>
+                                <th class="text-center">Action</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <?php for ($i = 1; $i <= 100; $i++) : ?>
+                                <tr class=" text-center ">
+                                    <td class="text-center"><?php echo $i; ?></td>
+                                    <td>Mohamed</td>
+                                    <td>Mushkir</td>
+                                    <td>mushkirmohamed@gmail.com</td>
+                                    <td class="text-center">0777195282</td>
+                                    <td class="d-flex align-items-center justify-content-center gap-3">
+                                        <a href="#" title="View Details" class=" text-success "><i class="fa-solid fa-circle-info"></i></a>
+                                        <a href="#" title="Edit Details" class=" text-primary "><i class="fa-solid fa-pen-to-square"></i></a>
+                                        <a href="#" title="Delete Detail" class=" text-danger "><i class="fa-solid fa-trash"></i></a>
+                                    </td>
+                                </tr>
+                            <?php endfor; ?>
+                        </tbody>
+                    </table>
+                </div>
+            </div>
+        </div>
+    </div>
+
+
+    <!-- Add new user modal -->
+    <!-- Modal -->
+    <div class="modal fade" id="addModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h1 class="modal-title fs-5" id="exampleModalLabel">Add New User</h1>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <div class="modal-body px-4 ">
+                    <form action="" method="post" id="form-data">
+                        <div class="form-group mb-3">
+                            <input type="text" name="fname" id="" placeholder="First Name" required class="form-control ">
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="text" name="lname" id="" placeholder="Last Name" required class="form-control ">
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="email" name="email" id="" placeholder="Email" required class="form-control ">
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="tel" name="phone" id="" placeholder="Phone" required class="form-control ">
+                        </div>
+                        <div class="form-group mb-3">
+                            <input type="submit" name="insert" class="btn btn-danger w-100 " value="Add User">
+                        </div>
+
+
+                    </form>
+                </div>
+                <!-- <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="button" class="btn btn-primary">Save changes</button>
+                </div> -->
+            </div>
+        </div>
+    </div>
+
+
+
+
+    <!-- Bootstrap CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.min.js" integrity="sha384-0pUGZvbkm6XF6gxjEnlmuGrJXVbNuzT9qBBavbLwCsOGabYfZo0T0to5eqruptLy" crossorigin="anonymous"></script>
+
+    <!-- JQuery -->
+    <script src="https://code.jquery.com/jquery-3.7.1.min.js" integrity="sha256-/JqT3SQfawRcv/BIHPThkBvs0OEvtFFmqPF/lYI/Cxo=" crossorigin="anonymous"></script>
+
+    <!-- Data Table JS CDN -->
+    <script src="https://cdn.datatables.net/v/bs5/dt-2.0.3/datatables.min.js"></script>
+
+    <!-- Sweet Alert CDN -->
+    <script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
+
+    <script>
+        $(document).ready(function() {
+
+            $("table").DataTable();
+        })
+    </script>
+
+
+</body>
+
+</html>
